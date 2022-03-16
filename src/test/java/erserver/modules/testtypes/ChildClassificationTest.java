d296b423be58a5dcdf884849358afe126aea2fdd
Corbatto Roberto <corbatto@eurisnet.it>
2022-03-16 14:36:48 +0100

    Code cleaning
    Pulizia codice test

--- src/test/java/erserver/modules/testtypes/ChildClassificationTest.java
+++ src/test/java/erserver/modules/testtypes/ChildClassificationTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ChildClassificationTest {
